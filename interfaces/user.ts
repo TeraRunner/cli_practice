ec8d473d3270ad7dd11c1b36e52a155f71e43480
RubenFrancoSanchez <ruben.franco.sanchez@cybg.com>
2022-09-28 11:45:56 +0100

    updating user without optionals

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,88 +1,90 @@
 type Name = {
-  title?: string;
-  first?: string;
-  last?: string;
+  title: string;
+  first: string;
+  last: string;
 };
 
 type Location = {
-  street?: Street;
-  city?: string;
-  state?: string;
-  country?: string;
-  postcode?: string | number;
-  coordinates?: Coordinates;
-  timezone?: Timezone;
+  street: Street;
+  city: string;
+  state: string;
+  country: string;
+  postcode: string | number;
+  coordinates: Coordinates;
+  timezone: Timezone;
 };
 
 type Street = {
-  number?: number;
-  name?: string;
+  number: number;
+  name: string;
 };
 
 type Coordinates = {
-  latitude?: string | number;
-  longitude?: string | number;
+  latitude: string | number;
+  longitude: string | number;
 };
 
 type Timezone = {
-  offset?: string;
-  description?: string;
+  offset: string;
+  description: string;
 };
 
 type Login = {
-  uuid?: string;
-  username?: string;
-  password?: string;
-  salt?: string;
-  md5?: string;
-  sha1?: string;
-  sha256?: string;
+  uuid: string;
+  username: string;
+  password: string;
+  salt: string;
+  md5: string;
+  sha1: string;
+  sha256: string;
 };
 
 type Dob = {
-  date?: string;
-  age?: number;
+  date: string;
+  age: number;
 };
 
 type Registered = {
-  date?: string;
-  age?: number;
+  date: string;
+  age: number;
 };
 
 type Id = {
-  name?: string;
-  value?: string | number | null;
+  name: string;
+  value: string | number | null;
 };
 
 interface PicturesUrl {
-  large?: string;
-  medium?: string;
-  thumbnail?: string;
+  large: string;
+  medium: string;
+  thumbnail: string;
 }
 
-type UserProperties =
+export type UserProperties =
   | string
   | number
-  | undefined
   | Name
   | Location
+  | Coordinates
+  | Timezone
   | Login
   | Dob
   | Registered
   | Id
-  | PicturesUrl;
+  | PicturesUrl
+  | null;
 
 export interface DefaultUser extends Record<string, UserProperties> {
-  gender?: string;
-  name?: Name;
-  location?: Location;
-  email?: string;
-  login?: Login;
-  dob?: Dob;
-  registered?: Registered;
-  phone?: string | number;
-  cell?: string | number;
-  id?: Id;
-  picture?: PicturesUrl;
-  nat?: string;
+  gender: string;
+  name: Name;
+  location: Location;
+  email: string;
+  login: Login;
+  dob: Dob;
+  registered: Registered;
+  phone: string | number;
+  cell: string | number;
+  id: Id;
+  picture: PicturesUrl;
+  nat: string;
 }
